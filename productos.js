31633fe3bbfe9b9dfcf9ec9ce670d5d45434269e
MercadoShop <139582844+omar12her@users.noreply.github.com>
2026-03-14 14:27:05 -0600

    Add files via upload

---
+++ productos.js
@@ -0,0 +1,39 @@
+const productos = [
+
+{
+nombre:"Taladro DEWALT",
+precio:2500,
+descripcion:"Taladro profesional de alto rendimiento",
+imagen:"img/taladro.jpeg",
+categoria:"herramientas",
+dias:30
+},
+
+{
+nombre:"Tenis Deportivos",
+precio:750,
+descripcion:"Tenis cómodos para correr",
+imagen:"https://cdn-icons-png.flaticon.com/512/2589/2589903.png",
+categoria:"zapatos",
+dias:15
+},
+
+{
+nombre:"Smartwatch",
+precio:850,
+descripcion:"Reloj inteligente con monitor de salud",
+imagen:"https://cdn-icons-png.flaticon.com/512/747/747376.png",
+categoria:"electronica",
+dias:7
+},
+
+{
+nombre:"Audífonos Bluetooth",
+precio:350,
+descripcion:"Audífonos inalámbricos con gran sonido",
+imagen:"https://cdn-icons-png.flaticon.com/512/727/727245.png",
+categoria:"electronica",
+dias:10
+}
+
+];
