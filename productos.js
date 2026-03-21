63dbed5c643830536075d0cabb668dfde11be70e
MercadoShop <139582844+omar12her@users.noreply.github.com>
2026-03-20 19:02:59 -0600

    Duplicate product entries in productos.js
    Added duplicate product entries for 'Luz nocturna decorativa', 'Tenis deportivos', and 'Smartwatch'.

--- productos.js
+++ productos.js
@@ -43,5 +43,39 @@ imagenes:[
 "https://cdn-icons-png.flaticon.com/512/747/747379.png"
 ]
 }
+  /////////2////////
+  {
+nombre:"Luz nocturna decorativa",
+precio:150,
+descripcion:"Para dormitorio, compañera para dormir",
+categoria:"Todos",
+imagenes:[
+  "img/o1.jpeg",
+  "img/02.jpeg",
+]
+},
+
+{
+nombre:"Tenis deportivos",
+precio:750,
+descripcion:"Ideales para correr",
+categoria:"zapatos",
+imagenes:[
+"https://cdn-icons-png.flaticon.com/512/2589/2589903.png",
+"https://cdn-icons-png.flaticon.com/512/2589/2589874.png"
+]
+},
+
+{
+nombre:"Smartwatch",
+precio:850,
+descripcion:"Reloj inteligente",
+categoria:"electronica",
+imagenes:[
+"https://cdn-icons-png.flaticon.com/512/747/747376.png",
+"https://cdn-icons-png.flaticon.com/512/747/747379.png"
+]
+}
+
 
 ];
